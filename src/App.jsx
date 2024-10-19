87024711bfd42cf130e23e67de8458c135d0aff6
AEPSchmitt <Tanketorskerogn@gmail.com>
2024-10-19 22:32:15 +0200

    New title

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import ScoreGraph from './ScoreGraph';
 export function App(props) {
   return (
     <div className='App'>
-      <h1>Quickgraphs</h1>
-      <ScoreGraph /> {/* Add the graph component here */}
+      <h1>Scoregraph</h1>
+      <ScoreGraph />
     </div>
   );
 }
